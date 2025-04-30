d4277ac0348e239dfd4239e62429eb6e9f171d85
James <j606888@gmail.com>
2025-04-30 15:01:30 +0800

    Add teachers index page

---
+++ src/app/teachers/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React from 'react';
+
+export default function CoursesPage() {
+
+  return (
+    <>
+      <div className="mx-auto px-3 py-6 flex flex-col gap-6 items-center justify-center md:px-6">
+        <h2 className='text-xl font-bold md:text-2xl'>認識我們團隊</h2>
+        <div className='flex flex-col gap-6 md:flex-row md:flex-wrap md:justify-center'>
+          <div className='w-[350px] h-[350px] relative cursor-pointer'>
+            <img src='/teachers/Sean.jpg' alt='Sean' className='w-full h-full object-cover rounded-lg' />
+            <div className='absolute bottom-3 left-3 right-3 bg-white/80 rounded-lg p-3'>
+              <h3 className='text-xl font-bold mb-1'>
+                Sean
+                <span className='text-sm text-gray-500'>（Baila&apos;more創辦人）</span>
+              </h3>
+              <div className='flex gap-2'>
+                {['Bachata Lv1', 'Bachata Lv2', 'Bachata 中階'].map((item) => (
+                  <div key={item} className='text-xs text-white bg-teal-600 p-2 rounded-md'>{item}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='w-[350px] h-[350px] relative cursor-pointer'>
+            <img src='/teachers/Kathy.jpg' alt='Kathy' className='w-full h-full object-cover rounded-lg' />
+            <div className='absolute bottom-3 left-3 right-3 bg-white/80 rounded-lg p-3'>
+              <h3 className='text-xl font-bold mb-1'>
+                Kathy
+              </h3>
+              <div className='flex gap-2'>
+                {['Lady Style', 'Bachata Lv2', 'Bachata 中階'].map((item) => (
+                  <div key={item} className='text-xs text-white bg-teal-600 p-2 rounded-md'>{item}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='w-[350px] h-[350px] relative cursor-pointer'>
+            <img src='/teachers/Angus.jpg' alt='Angus' className='w-full h-full object-cover rounded-lg' />
+            <div className='absolute bottom-3 left-3 right-3 bg-white/80 rounded-lg p-3'>
+              <h3 className='text-xl font-bold mb-1'>
+                Angus
+              </h3>
+              <div className='flex gap-2'>
+                {['Salsa Lv1', 'Salsa Lv2'].map((item) => (
+                  <div key={item} className='text-xs text-white bg-teal-600 p-2 rounded-md'>{item}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+} 
